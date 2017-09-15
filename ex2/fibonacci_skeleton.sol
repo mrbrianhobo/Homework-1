656ff7096fc51c1656d334c963152b4b31942599
Brian Ho <mr.brianhobo@hotmail.com>
2017-09-14 23:38:18 -0700

    yay

--- ex2/fibonacci_skeleton.sol
+++ ex2/fibonacci_skeleton.sol
@@ -3,8 +3,16 @@ pragma solidity ^0.4.16;
 
 contract Fibonacci {
 	function calculate(uint position) returns (uint result) {
-		/* Add one variable to hold our greeting */
+		require(position > 0);
+		if (position == 1) {
+			return 1;
+		} else if (position == 2) {
+			return 1;
+		} else {
+			return calculate(position - 1) + calculate(position - 2);
+		}
 	}
 
 	/* Add a fallback function to prevent contract payability and non-existent function calls */
+	function() { }
 }
